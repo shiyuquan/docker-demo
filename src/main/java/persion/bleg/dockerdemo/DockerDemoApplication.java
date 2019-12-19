70675525b46805458c32228da49b0105f12a9c69
shiyuquan <550765252@qq.com>
2019-12-19 15:47:56 +0800

    feat(log): 添加日志配置

--- src/main/java/persion/bleg/dockerdemo/DockerDemoApplication.java
+++ src/main/java/persion/bleg/dockerdemo/DockerDemoApplication.java
@@ -1,13 +1,27 @@
 package persion.bleg.dockerdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerDemoApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(DockerDemoApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(DockerDemoApplication.class, args);
+
+        log.debug("debug");
+
+        log.info("info");
+
+        log.error("error");
+
+        log.warn("warn");
+
+        log.trace("trace");
+
     }
 
 }
